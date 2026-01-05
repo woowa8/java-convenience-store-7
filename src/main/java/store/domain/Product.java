e233b40ce9b2a3299b02174c3a9d32cd6ba485d5
sunJ0120 <sspure1214@gmail.com>
2026-01-05 17:53:47 +0900

    feat: 상품 도메인 구현 완료

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,4 +1,31 @@
 package store.domain;
 
 public class Product {
+    private final String name;
+    private final int price;
+    private final int quantity;
+    private final Promotions promotion;
+
+    public Product(String name, int price, int quantity, Promotions promotions) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Promotions getPromotion() {
+        return promotion;
+    }
 }
