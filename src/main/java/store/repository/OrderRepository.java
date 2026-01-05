35711998fc9bb8911e8ae401592b2c1a46883197
sunJ0120 <sspure1214@gmail.com>
2026-01-05 17:54:21 +0900

    feat: 주문 레포지토리 구현 완료

--- src/main/java/store/repository/OrderRepository.java
+++ src/main/java/store/repository/OrderRepository.java
@@ -1,4 +1,22 @@
 package store.repository;
 
+import store.domain.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderRepository {
+    private List<Order> orders;
+
+    public OrderRepository() {
+        this.orders = new ArrayList<>();
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void addOrder(Order order){
+        this.orders.add(order);
+    }
 }
