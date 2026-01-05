ac9cd88501487dea305421ca4109a2fd4be0b32a
sunJ0120 <sspure1214@gmail.com>
2026-01-05 17:54:03 +0900

    feat: 프로모션 도메인 구현 완료

--- src/main/java/store/domain/Promotions.java
+++ src/main/java/store/domain/Promotions.java
@@ -1,4 +1,59 @@
 package store.domain;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Promotions {
+    private final String name;
+    private final int buyProduct;
+    private final int getProduct;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public Promotions(String name, int buyProduct, int getProduct, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buyProduct = buyProduct;
+        this.getProduct = getProduct;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    // 정적 팩토리 메서드로 매핑
+    public static Promotions of(
+            String name,
+            String buyProduct,
+            String getProduct,
+            String startDate,
+            String endDate
+    ) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        return new Promotions(
+                name,
+                Integer.parseInt(buyProduct),
+                Integer.parseInt(getProduct),
+                LocalDate.parse(startDate, formatter),
+                LocalDate.parse(endDate, formatter)
+        );
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuyProduct() {
+        return buyProduct;
+    }
+
+    public int getGetProduct() {
+        return getProduct;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
 }
