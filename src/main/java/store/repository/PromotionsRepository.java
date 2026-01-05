a6ae8d75f24616b2e0b76a7e4359f30a72b967c2
sunJ0120 <sspure1214@gmail.com>
2026-01-05 17:54:59 +0900

    feat: 프로모션 레포지토리 구현 완료

--- src/main/java/store/repository/PromotionsRepository.java
+++ src/main/java/store/repository/PromotionsRepository.java
@@ -1,4 +1,28 @@
 package store.repository;
 
+import store.domain.Promotions;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PromotionsRepository {
+    private final List<Promotions> promotions;
+
+    public PromotionsRepository() {
+        this.promotions = new ArrayList<>();
+    }
+
+    public List<Promotions> getPromotions() {
+        return promotions;
+    }
+
+    public void addPromotion(Promotions promotion) {
+        this.promotions.add(promotion);
+    }
+
+    public Promotions getPromotionByName(String name) {
+        return promotions.stream()
+                .filter(promotion -> promotion.getName().equals(name))
+                .findFirst().orElse(null);
+    }
 }
