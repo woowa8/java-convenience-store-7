41ecffc46feea7ab78cb2bb9608396a85d4a7018
sunJ0120 <sspure1214@gmail.com>
2026-01-06 19:13:46 +0900

    feat: checking을 위한 도메인 마련

--- src/main/java/store/dto/CheckOrderDto.java
+++ src/main/java/store/dto/CheckOrderDto.java
@@ -1,4 +1,21 @@
 package store.dto;
 
-public class CheckOrderDto {
+public record CheckOrderDto (
+        int promotionTotal,
+        int standardTotal,
+        String productName,
+        int addGift,
+        String promotionName,
+        int totalGet
+) {
+    public static CheckOrderDto of(int promotionTotal, int standardTotal, String productName, int addGift, String promotionName, int totalGet) {
+        return new CheckOrderDto(
+                promotionTotal,
+                standardTotal,
+                productName,
+                addGift,
+                promotionName,
+                totalGet
+        );
+    }
 }
