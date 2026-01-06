5e7012dfc5eb222f7f13a6fd4a44fb0f0ebcdc6a
sunJ0120 <sspure1214@gmail.com>
2026-01-06 19:13:20 +0900

    feat: 프로모션 도메인 마련

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,4 +1,42 @@
 package store.domain;
 
+import java.time.LocalDate;
+
 public class Promotion {
+    private final String name;
+    private final int buyAmount;
+    private final int getAmount;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    private Promotion(String name, int buyAmount, int getAmount, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buyAmount = buyAmount;
+        this.getAmount = getAmount;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public static Promotion of(String name, String buyAmount, String getAmount, String startDate, String endDate) {
+        return new Promotion(name, Integer.parseInt(buyAmount), Integer.parseInt(getAmount), LocalDate.parse(startDate), LocalDate.parse(endDate));
+    }
+
+    public  String getName() {
+        return name;
+    }
+    public int getBuyAmount() {
+        return buyAmount;
+    }
+
+    public int getGetAmount() {
+        return getAmount;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
 }
