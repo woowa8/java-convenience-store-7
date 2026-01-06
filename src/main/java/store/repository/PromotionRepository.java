ad7a9f2b590ea2692f583be27568eb35565654e0
sunJ0120 <sspure1214@gmail.com>
2026-01-06 19:16:05 +0900

    feat: 할인 정보 저장을 위한 레포지토리 마련

--- src/main/java/store/repository/PromotionRepository.java
+++ src/main/java/store/repository/PromotionRepository.java
@@ -1,4 +1,24 @@
 package store.repository;
 
-public class PromotionRepositoru {
+import store.domain.Promotion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromotionRepository {
+    private final List<Promotion> promotions;
+
+    public PromotionRepository() {
+        this.promotions = new ArrayList<>();
+    }
+
+    public void addPromotion(Promotion promotion) {
+        this.promotions.add(promotion);
+    }
+
+    public Promotion findPromotionByName(String name) {
+        return promotions.stream()
+                .filter(promotion -> promotion.getName().equals(name))
+                .findFirst().orElse(null);
+    }
 }
