827f0e53d963def5f0309db77cb5780ce79e1a6f
sunJ0120 <sspure1214@gmail.com>
2026-01-06 19:15:03 +0900

    feat: 증정품 저장을 위한 레포지토리 마련

--- src/main/java/store/repository/GiftRepository.java
+++ src/main/java/store/repository/GiftRepository.java
@@ -1,4 +1,27 @@
 package store.repository;
 
-public class GetRepository {
+import store.domain.Gift;
+import store.domain.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GiftRepository {
+    private final List<Gift> gifts;
+
+    public GiftRepository() {
+        gifts = new ArrayList<>();
+    }
+
+    public List<Gift> getGifts() {
+        return gifts;
+    }
+
+    public void addGift(Gift gift) {
+        gifts.add(gift);
+    }
+
+    public void clear() {
+        gifts.clear(); // 또는 orders.clear()
+    }
 }
