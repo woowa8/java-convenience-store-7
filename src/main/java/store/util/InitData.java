aab1c58eb502edfee2a4a98cb91b3f786fff59d3
sunJ0120 <sspure1214@gmail.com>
2026-01-06 19:17:56 +0900

    feat: 파일을 받기 위한 클래스

--- src/main/java/store/util/InitData.java
+++ src/main/java/store/util/InitData.java
@@ -1,4 +1,94 @@
 package store.util;
 
+import store.domain.Product;
+import store.domain.Promotion;
+import store.repository.ProductRepository;
+import store.repository.PromotionRepository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InitData {
+    private final PromotionRepository promotionRepository;
+    private final ProductRepository productRepository;
+
+    public InitData(PromotionRepository promotionRepository, ProductRepository productRepository) {
+        this.promotionRepository = promotionRepository;
+        this.productRepository = productRepository;
+    }
+
+    // 외부에서는 이것만 사용해서 데이터 초기화
+    public void init() {
+        try {
+            savePromotions();
+            saveProducts();
+        } catch (IOException e) {
+            throw new IllegalStateException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private List<List<String>> readFile(String fileName) throws IOException {
+        List<List<String>> mainList = new ArrayList<List<String>>();
+        // 1. 파일 읽어들이기
+        InputStream inputStream = InitData.class.getClassLoader().getResourceAsStream(fileName);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        reader.readLine();    // 첫 줄 스킵
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] lists = line.split(",");
+            List<String> subList = Arrays.asList(lists);
+            mainList.add(subList);
+        }
+        return mainList;
+    }
+
+    // 4. 각각 repository에 저장하기
+    private void savePromotions() throws IOException {
+        List<List<String>> mainList = readFile("promotions.md");
+        // name,buy,get,start_date,end_date 순서
+        for (List<String> list : mainList) {
+            Promotion promotion = Promotion.of(
+                    list.get(0),
+                    list.get(1),
+                    list.get(2),
+                    list.get(3),
+                    list.get(4)
+            );
+            promotionRepository.addPromotion(promotion);
+        }
+    }
+
+    private void saveProducts() throws IOException {
+        List<List<String>> mainList = readFile("products.md");
+        // name,price,quantity,promotion 순서
+        for (List<String> list : mainList) {
+            Product product = Product.of(
+                    list.get(0),
+                    list.get(1),
+                    list.get(2),
+                    promotionRepository.findPromotionByName(list.get(3))
+            );
+
+            productRepository.addProduct(product);
+
+            // TODO : 나도 이렇게 하고 싶지는 않았는데...
+            if("오렌지주스".equals(list.get(0)) || "탄산수".equals(list.get(0))){
+                Product product2 = Product.of(
+                        list.get(0),
+                        list.get(1),
+                        "0",
+                        null
+                );
+
+                productRepository.addProduct(product2);
+            }
+        }
+    }
 }
