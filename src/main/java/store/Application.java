464a38a959901379efc2df63b41fbe82105ba001
sunJ0120 <sspure1214@gmail.com>
2026-01-06 19:20:10 +0900

    feat: 최종 리드미 구현 완료

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,11 @@
 package store;
 
+import store.controller.OrderController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OrderController controller = new OrderController();
+        controller.run();
     }
 }
